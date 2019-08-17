ea3db0d7717d93db46544a09b29a58df0f783524
Arcangel Andres <artigue_arcangel@hotmail.com>
2019-08-17 16:46:32 -0300

    add numeric datatypes example

---
+++ src/main/java/NumericDataTypes.groovy
@@ -0,0 +1,19 @@
+class NumericDataTypes {
+    static void main(String [] args) {
+        //Notice that when using datatype, identifier def is not required
+
+        int x = 5
+        long y = 100l
+        float a = 10.56f
+        double b = 10.5e40
+        BigInteger bi = 30g
+        BigDecimal bd = 3.5g
+
+        println(x)
+        println(y)
+        println(a)
+        println(b)
+        println(bi)
+        println(bd)
+    }
+}
